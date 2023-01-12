25e54629664cdf4e58af6124693e24efe76331da
Jason Stout <stout.j.1373@gmail.com>
2023-01-11 21:24:34 -0600

    Setup Services

--- Sources/FBService/FBService.swift
+++
@@ -1,6 +0,0 @@
-public struct FBService {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/FBService/SignInWithApple/FirebaseSignInWithAppleUtils.swift
@@ -0,0 +1,10 @@
+//  Project:  
+// Created by Jason Stout on 1/11/23.
+//
+// Using Swift 5.0
+// Running on macOS 13.0
+// File
+//
+// "Design is not just what it looks like and feels like. Design is how it works." - Steve Jobs
+
+import Foundation
